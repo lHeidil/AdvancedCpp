4548277a73b89c63983f3c488716f95f8683b601
lHeidil <heidi.seif.2001@gmail.com>
2024-02-21 22:01:34 +0100

    initial commit
    exo 1,2,3 done

---
+++ Templates/Template.h
@@ -0,0 +1,99 @@
+#ifndef _TEMPLATES_H_
+#define _TEMPLATES_H_
+
+#include <iostream>
+#include <iomanip>
+#include <typeinfo.h>
+
+//Exo1 A
+//version 1
+//it may have issues when dealing with floating-point types due to potential precision errors when subtracting
+//il a besoin de operator -(TObj, TObj) , bool operator >(TObj, TObj) et TObj(TObj&) ctor copie
+//template <typename TObj> 
+//TObj MaxVal(TObj o1, TObj o2){
+//	if ((o1-o2)>0) return o1;
+//	else return o2;
+//}
+//version 2
+//besoin de bool operator >(TObj, TObj) et TObj(TObj&) ctor copie
+//template <typename TObj> 
+//TObj MaxVal(TObj o1, TObj o2){
+//	return o1>o2 ? o1:o2;
+//}
+//version 3
+template <typename TObj> inline 
+TObj& MaxVal(TObj& o1, TObj& o2){
+	cout<<"MaxVal Non Const\n";
+	return o1>o2 ? o1:o2;
+}
+//version 4
+template <typename TObj> inline 
+const TObj& MaxVal(const TObj& o1, const TObj& o2){
+	cout<<"MaxVal Const\n";
+	return o1>o2 ? o1:o2;
+}
+
+//Exo2 A
+//besoin de operator<<(ostream,TObj) pour chaque type TObj
+//passage par pointeur
+//Suitable for dynamically allocated arrays where the size is determined at runtime.
+template <typename TObj> inline 
+void ShowVect(std::ostream& os, const TObj* tab, size_t sz, const char * name=" "){
+	os<<"Tableau "<<name<<" de "<<sz<<" elements de type "<<typeid(TObj).name()<<"\n ";
+	for (size_t i=0; i<sz; ++i){
+		os<<tab[i]<<((i == sz-1)? "\n":", ");
+	}
+}
+//passage par référence
+//Suitable for arrays of fixed size known at compile time.
+template <typename TObj, const size_t SZ> inline 
+void ShowVect(std::ostream& os, const TObj (&tab)[SZ], const char * name=" "){
+	os<<"Tableau "<<name<<" de "<<SZ<<" elements de type "<<typeid(TObj).name()<<"\n ";
+	for (size_t i=0; i<SZ; ++i){
+		os<<tab[i]<<((i == SZ-1)? "\n":", ");
+	}
+}
+
+//Exo2 B,D,E
+//operator += (TObj, TObj)
+//TObj(TObj&) ctor copie
+//(TObj) ctor à 1 param 
+template <typename TObj, const size_t SZ> inline //pas pour les tableau dynamique
+TObj VectSomme(const TObj (&tab)[SZ]){
+	//TObj S = 0 ;
+	TObj S(tab[0]);
+	for (size_t i=1; i<SZ; ++i){
+		S += tab[i];
+	}
+	return S;
+}
+//Exo2 C
+template <typename TObj,size_t SZ> inline
+size_t GetSize(const TObj(& o1)[SZ]){ // () o1 pour etre fort que SZ
+	return sizeof(o1)/sizeof(o1[0]);
+}
+template <typename TObj, const size_t SZ> inline
+void TestVectSomme(std::ostream& os, const TObj (&tab)[SZ]){
+	os<<"Somme du tableau "<<VectSomme(tab)<<"\n ";
+}
+//Exo3 A
+template <typename TObj> inline
+void TriMontant(std::ostream& os, TObj* tab, size_t sz)
+{
+    for (size_t i = 0; i < sz - 1; ++i) {
+        for (size_t j = 0; j < sz - i - 1; ++j) {
+            if (tab[j] > tab[j + 1]) {
+				TObj temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < sz; ++i) {
+        os << tab[i] << ((i == sz - 1) ? "\n" : ", ");
+    }
+}
+//Exo3 B
+//bool operator >(TObj, TObj) et TObj(TObj&) ctor copie
+#endif
---
+++ Templates/main.cpp
@@ -0,0 +1,96 @@
+#include "Template.h"
+#include "Fract.h"
+using namespace std;
+using namespace Fract;
+
+void test1()
+{
+	
+	cout<<"************ Test 1 *****************\n";
+	//Exo1 B
+	float f1 = 5.3f, f2 = 4.5f;
+	double d1 = 2.25, d2 = -15.4;
+	int i1 = 25, i2 = -3;
+	CFract fr1(4,2),fr2(8,2);
+	cout<<MaxVal(f1, f2)<<endl;
+	cout<<MaxVal(d1, d2)<<endl;
+	cout<<MaxVal(i1, i2)<<endl;
+	cout<<MaxVal(i1, 23)<<endl; //ne fonctionne pas avec la version 3 car une constante ne passe pas par référence. Elle doit passer par référence constante . Voir version 4
+	//cout<<MaxVal(i1, f2)<<endl; // ambiguité sur le type TObj car deux chemins possibles. C'est à l'utilisateur de faire le choix
+	cout<<MaxVal(i1, (int)f2)<<endl;
+	cout<<MaxVal((float)i1, f2)<<endl;
+	cout<<MaxVal<float>(i1, f2)<<endl;
+	cout<<MaxVal<double>(i1, f2)<<endl;
+	MaxVal(i1, i2) = 89; // ne marche pas avec la version 4 à cause du const de la valeur de retour
+
+	//Exo1 C
+	/*The implicit requirement is the ability to perform
+	the (>) comparison operator, it should be defined for
+	the objects of type TObj.*/
+
+	//Exo1 D
+	cout<<MaxVal(fr1, fr2)<<endl;
+	//cout<<MaxVal<double>(fr1, d1)<<endl; //pas de conversion de CFract a double
+	cout<<MaxVal<CFract>(fr1, d1)<<endl; //conversion de double en int to be in CFract puis les comparer
+
+	cout<<"************ Fin de Test 1 *****************\n";
+}
+
+void test2()
+{
+	//Exo2 C
+	cout<<"************ Test 2 *****************\n";
+
+	double vd1[] = {2.25, -15.4, 6.6, -6.6};
+	//const size_t vd1_sz = sizeof(vd1)/sizeof(vd1[0]);
+	const size_t vd1_sz = GetSize(vd1);
+	ShowVect(cout, vd1, vd1_sz, "vd1");
+	ShowVect(cout, vd1, "vd1");
+	TestVectSomme(cout, vd1);
+
+	CFract vf1[]={CFract(7,12),CFract(3),CFract(1,6),CFract(2,13),CFract(2,5)};
+	//const size_t vf1_sz = sizeof(vf1)/sizeof(vf1[0]);
+	const size_t vf1_sz = GetSize(vf1);
+	ShowVect(cout, vf1, vf1_sz, "vf1");
+	ShowVect(cout, vf1, "vf1");
+	TestVectSomme(cout, vf1);
+
+	size_t tdyn_sz = 5 + (rand()%5);
+	int *tdyn = new int[tdyn_sz];
+	for (size_t i=0; i<tdyn_sz; ++i) tdyn[i] = int (i*i);
+	ShowVect( cout, tdyn, tdyn_sz, "tdyn");
+	//ShowVect( cout, tdyn, "tdyn");  // erreur avec le passage par référence car les tableaux dynamiques ne sont pas des tableaux qu'on peut passer par référence : impossible de convertir le paramètre 3 de 'const char [5]' en 'size_t'
+	delete[] tdyn;
+
+	cout<<"************ Fin de Test 2 *****************\n";
+}
+
+
+void test3()
+{
+	//Exo3 C
+	cout<<"************ Test 3 *****************\n";
+	int vint[] = {5, -2, 250, -250,0};
+	//const size_t vint_sz = sizeof(vint)/sizeof(vint[0]);
+	const size_t vint_sz = GetSize(vint);
+	TriMontant(cout, vint, vint_sz);
+
+	double vd1[] = {2.25, -15.4, 6.6, -6.6};
+	//const size_t vd1_sz = sizeof(vd1)/sizeof(vd1[0]);
+	const size_t vd1_sz = GetSize(vd1);
+	TriMontant(cout, vd1, vd1_sz);
+
+	CFract vf1[]={CFract(7,12),CFract(3),CFract(1,6),CFract(2,13),CFract(2,5)};
+	//const size_t vf1_sz = sizeof(vf1)/sizeof(vf1[0]);
+	const size_t vf1_sz = GetSize(vf1);
+	TriMontant(cout, vf1, vf1_sz);
+
+	cout<<"************ Fin de Test 3 *****************\n";
+}
+
+void main()
+{
+    test1();
+	test2();
+	test3();
+}
